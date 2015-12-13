ef26bf07f2a99262d1684f413b3055f7b863f21f
suruSharma <suruchi.sharma7389@gmail.com>
2015-12-12 22:56:49 -0500

    Jinja initial changes

--- guestbook/guestbook.py
+++ guestbook/guestbook.py
@@ -1,19 +1,38 @@
-from google.appengine.api import users
+import os
+import urllib
 
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
 
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        # Checks for active Google account session
+        #Checks for active Google session
         user = users.get_current_user()
-
         if user:
-            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-            self.response.write('Hello, ' + user.nickname())
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Sign out'
         else:
-            self.redirect(users.create_login_url(self.request.uri))
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Sign in'
+
+        template_values = {
+            'user': user,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
 
 app = webapp2.WSGIApplication([
